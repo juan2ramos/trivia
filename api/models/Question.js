12c338fb45148ac40cfe4b09486fa1f11b880afa
MaoMantish <mauricio@8manos.com>
2013-04-24 11:14:22 -0500

    cambio en query para obtener pregunta aleatoria
    no se hacia con JOINS sino con subqueries

--- api/models/Question.js
+++ api/models/Question.js
@@ -23,8 +23,9 @@ module.exports = {
 	},
 
 	getRandom: function (res, trivia_id, cb) {
-		var sql_query = 'SELECT question.id, question.question';
-		sql_query += ' FROM question, game WHERE question.id != game.question_id AND game.user_id = 1 AND question.trivia_id = '+trivia_id;
+		var sql_query = 'SELECT id, question FROM question';
+		sql_query += ' WHERE trivia_id = '+trivia_id;
+		sql_query += ' AND id NOT IN (SELECT question_id FROM game WHERE user_id = 1)';
 		sql_query += ' ORDER BY RAND() LIMIT 1';
 
 		this.query(sql_query, function(err, question) {
