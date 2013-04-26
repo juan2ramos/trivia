9cb842a4ab02bd05c24c86107bb09e5393bafde7
MaoMantish <mauricio@8manos.com>
2013-04-26 13:36:54 -0500

    redirect after sign up

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,10 +1,18 @@
 /*---------------------
-	:: User 
+	:: User
 	-> controller
 ---------------------*/
 var UserController = {
 
+	create: function (req,res) {
 
+		User.create({
+			name: req.param('name'),
+			password: req.param('password')
+		}).done(function(err, question) {
+			return res.redirect('/');
+		});
+	}
 
 };
 module.exports = UserController;
