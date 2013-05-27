d6259fc2117705a53a0a9d9ea175772b4f98895b
MaoMantish <mauricio@8manos.com>
2013-05-27 16:56:27 -0500

    env variable to control mysql migrations
    'safe' should be used in production

--- config/adapters.js
+++ config/adapters.js
@@ -37,6 +37,7 @@ module.exports.adapters = {
 		host		: process.env.MYSQL_HOST,
 		user		: process.env.MYSQL_USER,
 		password	: process.env.MYSQL_PASS,
-		database	: process.env.MYSQL_DB
+		database	: process.env.MYSQL_DB,
+		migrate		: process.env.MIGRATE || 'alter'
 	}
 };
