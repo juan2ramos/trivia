f7b205729879593dba1adee3eec92c693001d73a
MaoMantish <mauricio@8manos.com>
2013-07-12 11:05:49 -0500

    hookshot log to check if post receive hook is working

--- api/controllers/HookshotController.js
+++ api/controllers/HookshotController.js
@@ -2,6 +2,9 @@ var hookshot = require('hookshot');
 
 var HookshotController = {
 	index: function (req,res) {
+
+		console.log('hookshot pull and restart');
+
 		var branch = 'epa';
 		hookshot('refs/heads/'+branch, 'git pull origin '+branch+' && forever restart .app.js');
 		res.send(200);
