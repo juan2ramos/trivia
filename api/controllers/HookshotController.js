fe41de89822a4fb3411fa04a7567fdf5f0b7c0d9
MaoMantish <mauricio@8manos.com>
2013-07-15 10:24:07 -0500

    change in hookshot controller

--- api/controllers/HookshotController.js
+++ api/controllers/HookshotController.js
@@ -3,10 +3,16 @@ var hookshot = require('hookshot');
 var HookshotController = {
 	index: function (req,res) {
 
-		console.log('hookshot pull and restart');
-
 		var branch = 'epa';
 		hookshot('refs/heads/'+branch, 'git pull origin '+branch+' && forever restart .app.js');
+
+		hookshot().on('push', function(info) {
+			console.log('ref ' + info.ref + ' was pushed.');
+			if (info.ref == 'refs/heads/'+branch) {
+				console.log('hookshot pull and restart');
+			}
+		});
+
 		res.send(200);
 	}
 };
