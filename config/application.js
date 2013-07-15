b94f9c1d762dbe01fdaccedc7c442fdbd47c8170
MaoMantish <mauricio@8manos.com>
2013-07-15 11:59:23 -0500

    removed pssport session middleware changed hookshot url

--- config/application.js
+++ config/application.js
@@ -93,10 +93,9 @@ module.exports = {
 		customMiddleware: function(app)
 		{
 			var branch = 'epa';
-			app.use('/hksht', hookshot('refs/heads/'+branch, 'git pull origin '+branch+' && forever restart .app.js'));
+			app.use('/hookshot', hookshot('refs/heads/'+branch, 'git pull origin '+branch+' && forever restart .app.js'));
 
 			app.use(passport.initialize());
-			app.use(passport.session());
 		}
 	}
 
